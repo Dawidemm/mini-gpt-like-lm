691f1ca4a0ccadf572a06811b098afc5012811d5
Dawidemm <dawid.mazur@icloud.com>
2025-01-17 00:46:37 +0100

    feat: Add raw text loader.

---
+++ minigpt/utils/raw_text_loader.py
@@ -0,0 +1,40 @@
+import os
+from typing import List
+from enum import Enum
+
+
+class SpecialToken(Enum):
+    SPECIALTOKEN = "<|endoftext|>"
+
+
+class RawTextLoader():
+    def __init__(
+            self,
+            dataset_path: str,
+            special_token: str = SpecialToken.SPECIALTOKEN.value
+    ):
+        self.dataset_path = dataset_path
+        self.file_list = self._get_file_list()
+        self.special_token = special_token
+
+    def _get_file_list(self) -> List:
+
+        if not os.path.exists(self.dataset_path):
+            raise FileNotFoundError(f"The folder '{self.dataset_path}' does not exist.")
+        
+        files = [f for f in os.listdir(self.dataset_path) if os.path.isfile(os.path.join(self.dataset_path, f))]
+        return files
+    
+    def load_text(self) -> str:
+
+        print(self.file_list)
+        
+        combined_text = []
+        for filename in self.file_list:
+            file_path = os.path.join(self.dataset_path, filename)
+            print(file_path)
+            with open(file_path, "r", encoding="utf-8") as f:
+                text = f.read()
+                combined_text.append(text)
+
+        return f" {self.special_token} ".join(combined_text)
