2f5cb1ca2d6b263ee26d15f12623669d5d432bb0
Dawidemm <dawid.mazur@icloud.com>
2025-02-01 00:59:42 +0100

    feat: Change model params to gpt2 params.

--- minigpt/models/settings.py
+++ minigpt/models/settings.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class MiniGPTSettings:
     vocabulary_size: int = 50257
-    context_length: int = 512
-    embeddings_dim: int = 512
-    num_heads: int = 8
-    num_layers: int = 4
+    context_length: int = 1024
+    embeddings_dim: int = 768
+    num_heads: int = 12
+    num_layers: int = 12
     dropout_rate: float = 0.1
