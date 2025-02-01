b21029bd7a349ea0270e8769550081b62fa23c04
Dawidemm <dawid.mazur@icloud.com>
2025-02-01 20:57:23 +0100

    feat: Lower context length.

--- minigpt/models/settings.py
+++ minigpt/models/settings.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class MiniGPTSettings:
     vocabulary_size: int = 50257
-    context_length: int = 1024
+    context_length: int = 256
     embeddings_dim: int = 768
     num_heads: int = 12
     num_layers: int = 12
