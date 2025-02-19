ffce30878888a34d80626f7aef8efc2243c7cb4d
Dawidemm <dawid.mazur@icloud.com>
2025-02-19 01:19:47 +0100

    refactor: Minor changes.

--- minigpt/app/fastapi_app.py
+++ minigpt/app/fastapi_app.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-# from app.routers import chat
+from minigpt.app.routers import chat
 
 app = FastAPI(title="MiniGPT Chat API")
 
-# Dodanie routera dla endpointów czatu
-# app.include_router(chat.router)
+app.include_router(chat.router)
 
 @app.get("/")
 async def root():
